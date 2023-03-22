ed2e6cb0d2ec2c32faca91420ef0e4fdd0c8bf74
IKBOstudent <maxpon225@gmail.com>
2023-03-23 00:18:15 +0300

    v0.1.0 added test functionality

--- main.py
+++ main.py
@@ -1,41 +1,67 @@
-import json
-
-
-import requests
+import os
+import telebot
+import telebot.types as tt
 from dotenv import load_dotenv
 
-from handlers import bot_service
-from utils import CachedSession, JSONFileCache
+from utils import FilmFetch
 
-load_dotenv()
+load_dotenv()  # .env variables
+
+bot_service = telebot.TeleBot(os.environ['BOT_TOKEN'])  # bot instance
 
 
-class FilmFetch:
-    def __init__(self, url, headers, params):
-        self.url = url
-        self.headers = headers
-        self.params = params
+@bot_service.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    """Welcome message"""
+    bot_service.send_message(message.chat.id, "Это бот, который поможет найти нужный фильм или сериал :)")
 
-    def cached_request(self):
-        custom_backend = JSONFileCache('cached_requests', serializer='json')
-        session = CachedSession(backend=custom_backend)
-        data = session.get(self.url, headers=self.headers, params=self.params).json()
-        return data
 
-    def standard_request(self):
-        data = requests.get(self.url, headers=self.headers, params=self.params).json()
-        return data
+@bot_service.message_handler(commands=['test'])
+def test_message(message):
+    """test buttons"""
+    markup = tt.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = tt.KeyboardButton("Кнопка Раз")
+    item2 = tt.KeyboardButton("Кнопка Два")
+    markup.add(item1, item2)
+    bot_service.send_message(message.chat.id, 'Выберите из меню', reply_markup=markup)
+
+
+@bot_service.message_handler(commands=['find'])
+def send_film_test(message):
+    """test film"""
+
+    url = 'https://api.kinopoisk.dev/v1/movie'
+    headers = {"X-API-KEY": os.environ["API_KEY"]}
+    params = {'id': 335}
+    Fetch = FilmFetch(url, headers, params)
+    try:
+        info = Fetch.cached_request()['docs'][0]  # кэшированный запрос
+        poster = info['poster']['url']
+        caption = f"Фильм: {info['name']}\n" \
+                  f"Рейтинг: {info['rating']['kp']}\n" \
+                  f"Год: {info['year']}\n\n" \
+                  f"{info['description']}"
+
+        markup = tt.InlineKeyboardMarkup()
+
+        if info['externalId']['kpHD'] is not None:
+            btn_my_site = tt.InlineKeyboardButton(
+                text='Смотреть онлайн',
+                url=f"https://hd.kinopoisk.ru/film/{info['externalId']['kpHD']}")
+            markup.add(btn_my_site)
+
+        bot_service.send_photo(message.chat.id, photo=poster, caption=caption, reply_markup=markup)
+    except Exception:
+        print("Request unsuccessful")
+
+
+@bot_service.message_handler(func=lambda m: True)
+def echo_all(message):
+    """Reply for any other message"""
+    bot_service.reply_to(message, "Не понял :/")
 
 
 if __name__ == "__main__":
-    # url = 'https://api.kinopoisk.dev/v1/movie'
-    # headers = {"X-API-KEY": os.API_KEY}
-    # params = {
-    #     'id': 335
-    # }
-    # MementoFetch = FilmFetch(url, headers, params)
-    # print(MementoFetch.cached_request())
-
     print("bot running")
     bot_service.infinity_polling()
 
---
+++ utils/fetch.py
@@ -0,0 +1,49 @@
+import json
+from os.path import splitext
+
+import requests
+from requests_cache import CachedSession, FileCache, FileDict
+
+
+class JSONFileCache(FileCache):
+    def __init__(self, cache_name, **kwargs):
+        super().__init__(cache_name, **kwargs)
+        self.responses = JSONFileDict(cache_name, **kwargs)
+
+
+class JSONFileDict(FileDict):
+    def __setitem__(self, key: str, value: requests.Response):
+        super().__setitem__(key, value)
+        response_path = splitext(self._path(key))[0]
+        json_path = f'{response_path}_content.json'
+
+        with self._try_io(ignore_errors=True):
+            content = json.dumps(value.json(), indent=2)
+            with open(json_path, 'w') as f:
+                f.write(content)
+
+
+class FilmFetch:
+    def __init__(self, url, headers, params):
+        self.url = url
+        self.headers = headers
+        self.params = params
+
+    def cached_request(self):
+        """
+        Request with CachedSession.
+            Response not found in cache ->
+            sends HTTP request. Then puts response in cache.
+
+            Response found in cache -> gets data from cache.
+        """
+
+        custom_backend = JSONFileCache('cached_requests', serializer='json')
+        session = CachedSession(backend=custom_backend)
+        data = session.get(self.url, headers=self.headers, params=self.params).json()
+        return data
+
+    def standard_request(self):
+        """Standard HTTP request"""
+        data = requests.get(self.url, headers=self.headers, params=self.params).json()
+        return data
