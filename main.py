f8bfe1ed9d7566ac1c2223aa4eb36e5e95e01009
hvdrtsoul <ragelar@yandex.ru>
2023-05-22 16:00:22 +0300

    added review search (callback button) and search by actor name

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ from dotenv import load_dotenv
 
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, \
-    filters, ConversationHandler
+    filters, ConversationHandler, CallbackQueryHandler
 
 from handlers.SearchHandlers import SearchHandlers
 from handlers.TestHandlers import TestHandlers
 from handlers.DefaultHandlers import DefaultHandlers
-
 
 load_dotenv()
 
@@ -37,21 +36,32 @@ class STATES:
     """states for conversations"""
     START = 0
     FIND_BY_NAME = 1
+    FIND_REVIEWS = 2
+    FIND_BY_ACTOR = 3
 
 
 default_markup = ReplyKeyboardMarkup([
-            ['/find', '/help'],
-            ['/test_keyboard']
-        ], resize_keyboard=True)
+    ['/find', '/help', '/actor'],
+    ['/test_keyboard']
+], resize_keyboard=True)
 
 default = DefaultHandlers(logger, default_markup)
 test = TestHandlers(logger)
 search = SearchHandlers(STATES, logger)
 
 
+async def keyboard_callback(update, context):
+    query = update.callback_query
+    # print('query:', query)
+
+    print('query.data:', query.data)
+    query.answer(f'selected: {query.data}')
+
+
 application.add_handler(CommandHandler("start", default.send_welcome))
 application.add_handler(CommandHandler("help", default.send_help))
 application.add_handler(CommandHandler("test_keyboard", test.test_keyboard))
+#application.add_handler(CallbackQueryHandler(keyboard_callback))
 
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -65,7 +75,21 @@ conv_handler = ConversationHandler(
     states={
         STATES.START: [],
         STATES.FIND_BY_NAME: [
-            MessageHandler(filters.TEXT & ~filters.COMMAND, search.response_film)
+            MessageHandler(filters.TEXT & ~filters.COMMAND, search.response_film),
+            CallbackQueryHandler(search.review_callback)
+        ],
+    },
+    fallbacks=[CommandHandler("cancel", cancel)],
+)
+
+application.add_handler(conv_handler)
+
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler('actor', search.send_actor)],
+    states={
+        STATES.START: [],
+        STATES.FIND_BY_ACTOR: [
+            MessageHandler(filters.TEXT & ~filters.COMMAND, search.response_actor),
         ],
     },
     fallbacks=[CommandHandler("cancel", cancel)],
@@ -79,7 +103,6 @@ async def send_unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Не понял :/")
 
 
-
 application.add_handler(MessageHandler(filters.Regex(r'.*'), send_unknown))
 
 if len(sys.argv) > 1 and sys.argv[1] == "PRODUCTION":
@@ -87,3 +110,4 @@ if len(sys.argv) > 1 and sys.argv[1] == "PRODUCTION":
 else:
     print("starting bot...")
     application.run_polling(drop_pending_updates=True)
+    application.updater.initialize()
